8664e88bb1586d744ce596bafc09bd7858b07ad6
ramchitturi281 <chitturisriram557@gmail.com>
2022-07-13 10:22:45 +0530

    Update char_count.py

--- char_count.py
+++ char_count.py
@@ -8,3 +8,14 @@ for i in str:
         
         
 print(count)
+
+
+str = "eeeeeE"
+count = 0
+for i in str:
+    if i in ('e','E'):
+        count+=1
+        
+print(count)
+
+#https://stackoverflow.com/questions/39560387/count-a-characterboth-uppercase-and-lowercase-in-a-string-using-a-for-loop-in
